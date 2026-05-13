1cc59c3c0e0b0983a8673f67a312036a02f4ed0a
Kai Nguyen <kngu21@byu.edu>
2026-05-13 03:09:02 -0600

    Initiated implementation of en pesant and castling.

--- shared/src/main/java/chess/ChessPiece.java
+++ shared/src/main/java/chess/ChessPiece.java
@@ -14,6 +14,7 @@ public class ChessPiece {
     private final ChessGame.TeamColor pieceColor;
     private final ChessPiece.PieceType type;
     private int counter;
+    private boolean doubleMove;
 
     @Override
     public boolean equals(Object o) {
@@ -33,6 +34,7 @@ public class ChessPiece {
         this.pieceColor = pieceColor;
         this.type = type;
         this.counter = 0;
+        this.doubleMove = false;
     }
 
     /**
