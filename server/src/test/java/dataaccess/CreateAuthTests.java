7c9ecd5e9f4efcb70432af8e3a93b0999671806c
Kai Nguyen <kngu21@byu.edu>
2026-05-27 13:40:27 -0600

    Getting rid of duplicate code

--- server/src/test/java/dataaccess/CreateAuthTests.java
+++ server/src/test/java/dataaccess/CreateAuthTests.java
@@ -3,6 +3,7 @@ package dataaccess;
 import model.UserData;
 import org.junit.jupiter.api.Test;
 import service.AlreadyTakenException;
+import service.UnauthorizedException;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -12,14 +13,18 @@ public class CreateAuthTests extends BaseTest{
         service2.register(new UserData("kai", "password", "cool@email.com"));
         assertDoesNotThrow(() ->{
             service2.register(new UserData("jim", "howdy", "coolio@email.com"));
+            var login = service2.login("kai", "password");
+            auth.getAuth(login.authToken());
         });
     }
     @Test
     void failedCreateAuth() throws AlreadyTakenException, DataAccessException {
         service2.register(new UserData("kai", "password", "cool@email.com"));
-
-        assertThrows(AlreadyTakenException.class, () -> {
-            service2.register(new UserData("kai", "password", "cool@email.com"));
+        var first = service2.login("kai", "password");
+        assertThrows(UnauthorizedException.class, () -> {
+            service2.register(new UserData("jim", "howdy", "cool@email.com"));
+            var second = service2.login("jim", "howdy");
+            service2.logout(second.authToken() + "i");
         });
     }
 }
