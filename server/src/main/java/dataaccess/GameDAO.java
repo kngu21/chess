1b3d66d4c1afc9d400f76208a5d89a295a77141c
Kai Nguyen <kngu21@byu.edu>
2026-05-27 04:26:08 -0600

    Halfway done with gamedataaccess methods.

--- server/src/main/java/dataaccess/GameDAO.java
+++ server/src/main/java/dataaccess/GameDAO.java
@@ -5,8 +5,8 @@ import java.sql.SQLException;
 import java.util.ArrayList;
 
 public interface GameDAO {
-    GameData createGame(String username);
-    GameData getGame(int gameID);
+    GameData createGame(String username) throws DataAccessException;
+    GameData getGame(int gameID) throws DataAccessException;
     ArrayList<GameData> listGames();
     void replaceGame(GameData newGame);
     void clear() throws SQLException, DataAccessException;
--- server/src/main/java/dataaccess/MySQLGameDataAccess.java
+++ server/src/main/java/dataaccess/MySQLGameDataAccess.java
@@ -1,4 +1,6 @@
 package dataaccess;
+import chess.ChessGame;
+import com.google.gson.Gson;
 import model.GameData;
 import java.sql.*;
 import java.util.ArrayList;
@@ -7,27 +9,63 @@ import static dataaccess.DatabaseManager.configureDatabase;
 
 public class MySQLGameDataAccess implements GameDAO{
 
+    private int gameID;
     private String[] createStatements = {
             """ 
               CREATE TABLE IF NOT EXISTS  games (
-              `gameID` int NOT NULL AUTO_INCREMENT,
-              `gameData` TEXT NOT NULL,
+              `gameID` int NOT NULL,
+              `whiteUsername` varchar(128) NOT NULL,
+              `blackUsername` varchar(128) NOT NULL,
+              `gameName` varchar(128) NOT NULL,
+              `chessGame` TEXT NOT NULL,
               PRIMARY KEY (`gameID`)
             ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci
             """
     };
 
     public MySQLGameDataAccess() throws SQLException, DataAccessException {
+        this.gameID = 1;
         configureDatabase(createStatements);
     }
 
     @Override
-    public GameData createGame(String username) {
-        return null;
+    public GameData createGame(String gameName) throws DataAccessException {
+        ChessGame game = new ChessGame();
+        GameData newGame = new GameData(gameID, null, null, gameName, game);
+        try (Connection conn = DatabaseManager.getConnection()) {
+            var statement = "INSERT INTO users (gameID, whiteUsername, blackUsername, gameName, chessGame) VALUES (?, ?, ?, ?)";
+            try (PreparedStatement ps = conn.prepareStatement(statement)) {
+                ps.setInt(1, gameID);
+                ps.setString(2, null);
+                ps.setString(3, null);
+                ps.setString(4, gameName);
+                ps.setString(5, new Gson().toJson(game));
+                ps.executeUpdate();
+                gameID++;
+            }
+        } catch (SQLException e) {
+            throw new DataAccessException("Unable to insert userData");
+        }
+        return newGame;
     }
 
     @Override
-    public GameData getGame(int gameID) {
+    public GameData getGame(int gameID) throws DataAccessException {
+        try (Connection conn = DatabaseManager.getConnection()) {
+            var statement = "SELECT whiteUsername, blackUsername, gameName, chessGame FROM games WHERE gameID=?";
+            try (PreparedStatement ps = conn.prepareStatement(statement)) {
+                ps.setInt(1, gameID);
+                try (ResultSet rs = ps.executeQuery()){
+                    if(rs.next()){
+                        ChessGame game = new Gson().fromJson(rs.getString("chessGame"), ChessGame.class);
+                        return new GameData(gameID, rs.getString("whiteUsername"), rs.getString("blackUsername"), rs.getString("gameName"), game);
+                    }
+                }
+            }
+
+        } catch (SQLException e) {
+            throw new DataAccessException("Unable to get gameData");
+        }
         return null;
     }
 
@@ -44,14 +82,12 @@ public class MySQLGameDataAccess implements GameDAO{
     @Override
     public void clear() throws SQLException, DataAccessException {
         try (Connection conn = DatabaseManager.getConnection()) {
-            var statement = "DROP TABLE games";
+            var statement = "TRUNCATE TABLE games";
             try (PreparedStatement ps = conn.prepareStatement(statement)) {
                 ps.executeUpdate();
             }
-
         } catch (SQLException e) {
-            throw new DataAccessException("Unable to remove table games");
+            throw new DataAccessException("Unable to clear table games");
         }
-        DatabaseManager.configureDatabase(createStatements);
     }
 }
